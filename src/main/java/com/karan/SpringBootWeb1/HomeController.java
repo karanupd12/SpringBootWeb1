c0a2c9d1baa0a1e296d30414e9549861080b2774
karan Upadhyay <karanupd12@gmail.com>
2025-05-18 15:48:41 +0530

    View made, ModelAndView used

--- src/main/java/com/karan/SpringBootWeb1/HomeController.java
+++ src/main/java/com/karan/SpringBootWeb1/HomeController.java
@@ -1,10 +1,13 @@
 package com.karan.SpringBootWeb1;
 
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -12,14 +15,17 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         System.out.println("home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int n1, @RequestParam("num2") int n2, HttpSession session) {
+    public ModelAndView add(@RequestParam("num1") int n1, @RequestParam("num2") int n2, ModelAndView mv) {
         System.out.println("add method called");
+
         int result = n1 + n2;
-        session.setAttribute("result", result);
-        return "result.jsp";
+        mv.addObject("result", result);
+        mv.setViewName("result");
+
+        return mv;
     }
 }
